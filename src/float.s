cb1ab2c41669e08db2707f4877a73f3af05784b6
Jeremy@JYL <jeremylow823@outlook.com>
2024-08-05 14:36:11 +0800

    Added floating point examples

---
+++ src/float.s
@@ -0,0 +1,23 @@
+.global _start
+.extern printf
+
+.data
+	format: .asciz "%f\n"
+	f:	.float 1.123
+
+.text
+	_start:
+		ldr 	x0, =format
+		ldr 	x2, =f
+		ldr 	s0, [x2]
+		fcvt 	d0, s0
+		mov 	x1, #0
+	
+		bl 	printf
+		b	exit
+
+	exit:
+		mov	x8, #93
+		mov 	x0, #0
+		svc 	0
+
